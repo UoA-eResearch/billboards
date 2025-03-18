92afead61495c88f181c0a1b71d7f57508f2a37b
Nick Young <nick.young@auckland.ac.nz>
2025-03-18 17:03:03 +1300

    update geocode_ARLA script to use new Places API

--- geocode_ARLA.py
+++ geocode_ARLA.py
@@ -1,22 +1,51 @@
 #!/usr/bin/env python3
 import json
-import googlemaps
+import requests
 import sys
 import os
 import pandas as pd
+pd.set_option('display.max_columns', None)
 from tqdm import tqdm
+from pprint import pprint
+from dotenv import load_dotenv
+load_dotenv()
+
 tqdm.pandas()
-
-gmaps = googlemaps.Client(key=os.environ["GOOGLE_API_KEY"])
-print(gmaps)
-
-df = pd.read_excel("misc/May-2021-Licences.xlsx", skiprows=1)
-df = df[df.LicenceType == "Off-licence"]
+df = pd.read_excel("February-2025-Licences.xlsx", skiprows=1)
 
 cols = ["Premises Name", "Premises Street", "Premises Suburb", "Premises City"]
-df["address"] = df.progress_apply(lambda row: 
+df["address"] = df.apply(lambda row: 
     ", ".join([row[k] for k in cols if not pd.isna(row[k])]),
     axis=1
 )
-df["geo"] = df.address.progress_apply(lambda address: gmaps.geocode(address, components={"country": "NZ"}))
-df.to_json("data/ARLA.json", 'records')
+print(df.address)
+def geocode(address):
+    try:
+        r = requests.post('https://places.googleapis.com/v1/places:searchText', json={
+                "textQuery" : address,
+                "pageSize": 1,
+                "locationRestriction": {
+                    "rectangle": {
+                        #158.185443,-52.720720,187.811214,-30.693290
+                        "low": {
+                            "latitude": -52.720720,
+                            "longitude": 158.185443
+                        },
+                        "high": {
+                            "latitude": -30.693290,
+                            "longitude": 180
+                        }
+                    }
+                }
+            }, headers={
+                'X-Goog-Api-Key': os.environ["GOOGLE_API_KEY"],
+                'X-Goog-FieldMask': 'places.location,places.displayName,places.formattedAddress,places.servesBeer,places.servesWine'
+        })
+        return r.json()["places"][0]
+    except Exception as e:
+        print(e)
+        return None
+
+results = pd.json_normalize(df.address.progress_apply(geocode))
+print(results)
+results.to_csv("data/ARLA.csv")
